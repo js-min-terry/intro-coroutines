11d306180a0a9b156fba9d36fcec30842cb620d3
terry <js.min@toss.im>
2020-08-24 09:09:47 +0900

    request-blocking (#1)

--- src/tasks/Aggregation.kt
+++ src/tasks/Aggregation.kt
@@ -14,5 +14,8 @@ TODO: Write aggregation code.
  The corresponding test can be found in test/tasks/AggregationKtTest.kt.
  You can use 'Navigate | Test' menu action (note the shortcut) to navigate to the test.
 */
-fun List<User>.aggregate(): List<User> =
-    this
+fun List<User>.aggregate(): List<User> {
+    return this.groupBy { it.login }
+        .map { User(login = it.key, contributions = it.value.sumBy { user: User -> user.contributions }) }
+        .sortedByDescending { it.contributions }
+}
--- src/tasks/Request1Blocking.kt
+++ src/tasks/Request1Blocking.kt
@@ -3,12 +3,14 @@ package tasks
 import contributors.*
 import retrofit2.Response
 
+// ui 쓰레드에서 사용되기 때문에 ui가 freeze된다.
+// AWT-EventQueue-0: Swing, AWT event dispatching thread
 fun loadContributorsBlocking(service: GitHubService, req: RequestData) : List<User> {
     val repos = service
-        .getOrgReposCall(req.org)
-        .execute() // Executes request and blocks the current thread
+        .getOrgReposCall(req.org) // Call 객체를 리턴함
+        .execute() // Call 객체를 실행. Executes request and blocks the current thread
         .also { logRepos(req, it) }
-        .body() ?: listOf()
+        .bodyList()
 
     return repos.flatMap { repo ->
         service
