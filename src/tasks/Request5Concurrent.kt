12369cbf184a0bd2449f4284b31a400054852637
terry <js.min@toss.im>
2020-08-24 10:40:20 +0900

    concurrent (#5)

--- src/tasks/Request5Concurrent.kt
+++ src/tasks/Request5Concurrent.kt
@@ -4,5 +4,16 @@ import contributors.*
 import kotlinx.coroutines.*
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> = coroutineScope {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList()
+
+    repos.map { repo ->
+        // Dispatchers 설정을 통해서 코루틴이 실행될 쓰레드풀을 설정할 수 있다.
+        async(Dispatchers.Default) {
+            log("starting loading for ${repo.name}")
+            service.getRepoContributors(req.org, repo.name).also { logUsers(repo, it) }.bodyList()
+        }
+    }.awaitAll().flatten().aggregate()
 }
